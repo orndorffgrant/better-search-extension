be4aed405e5281f3c1e957f365433d2ae245a681
Grant Orndorff <grant.orndorff@canonical.com>
2021-10-20 17:08:53 -0400

    prettier

--- newtab.js
+++ newtab.js
@@ -12,10 +12,18 @@ search.onchange = () => {
     resultsContainer.innerHTML = "";
     results.forEach(result => {
       const resultElement = resultTemplate.content.cloneNode(true);
-      const a = resultElement.querySelector("a");
+      const a = resultElement.querySelector(".link-title");
       a.href = result.url;
       a.innerText = result.title;
+      const urlspan = resultElement.querySelector(".link");
+      urlspan.innerText = result.url;
       resultsContainer.appendChild(resultElement);
     })
   })
 };
+
+if (document.activeElement !== search) {
+  console.log("aha!");
+  // or just location.reload()?
+  (async function() {browser.tabs.reload((await browser.tabs.getCurrent()).id)})()
+}
