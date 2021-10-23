7bad0c2f73b8b8a855140216bcf5ceb5b9faa9ce
Grant Orndorff <grant.orndorff@canonical.com>
2021-10-22 21:13:35 -0400

    search as you type

--- newtab.js
+++ newtab.js
@@ -2,7 +2,7 @@ const search = document.getElementById("search");
 const resultsContainer = document.getElementById("results");
 const resultTemplate = document.querySelector("#result");
 
-search.onchange = () => {
+const onchange = () => {
   browser.runtime.sendMessage({
     type: "lookup",
     args: {
@@ -10,20 +10,29 @@ search.onchange = () => {
     },
   }).then((results) => {
     resultsContainer.innerHTML = "";
-    results.forEach(result => {
-      const resultElement = resultTemplate.content.cloneNode(true);
-      const a = resultElement.querySelector(".link-title");
-      a.href = result.url;
-      a.innerText = result.title;
-      const urlspan = resultElement.querySelector(".link");
-      urlspan.innerText = result.url;
-      resultsContainer.appendChild(resultElement);
-    })
+    if (results.length > 0) {
+      results.forEach(result => {
+        const resultElement = resultTemplate.content.cloneNode(true);
+        const a = resultElement.querySelector(".link-title");
+        a.href = result.url;
+        a.innerText = result.title;
+        const urlspan = resultElement.querySelector(".link");
+        urlspan.innerText = result.url;
+        resultsContainer.appendChild(resultElement);
+      })
+    } else {
+      resultsContainer.innerHTML = "no results";
+    }
+  }).catch((e) => {
+      resultsContainer.innerHTML = "error: " + e;
   })
 };
 
-if (document.activeElement !== search) {
-  console.log("aha!");
-  // or just location.reload()?
-  (async function() {browser.tabs.reload((await browser.tabs.getCurrent()).id)})()
-}
+let debounce;
+search.onkeyup = () => {
+  clearTimeout(debounce);
+  debounce = setTimeout(() => {
+    onchange();
+  }, 200);
+};
+
